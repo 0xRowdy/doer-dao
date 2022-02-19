5f2649d3fb7682fb5b6e0135d0d27506953ec1b9
Mark Roddy <mroddy@deloitte.com>
2022-02-19 10:05:38 -0700

    Created mock data for UI

---
+++ proposals-mock/proposals.js
@@ -0,0 +1,5 @@
+export const mockProposals = [
+  { id: 00001, name: "DoerDao1", proposedFunds: "1000", hasSubProposals: true },
+  { id: 00001, name: "DoerDao1", proposedFunds: "1000", hasSubProposals: true },
+  { id: 00001, name: "DoerDao1", proposedFunds: "1000", hasSubProposals: true },
+];
