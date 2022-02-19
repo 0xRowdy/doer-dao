dea9faab0a7e772e3956f2d110bac80300a85c54
Anar Enhsaihan <aenhsaihan@gmail.com>
2022-02-19 16:32:18 -0700

    Refactor Action into a separate contract file

--- contracts/ActionFactory.sol
+++ contracts/ActionFactory.sol
@@ -2,6 +2,8 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+import "./Action.sol";
+
 contract ActionFactory {
     mapping(address => address) public parents;
 
@@ -16,15 +18,5 @@ contract ActionFactory {
         parents[address(action)] = _parent;
 
         emit CreateAction(address(action), _parent, _data);
-    }
-}
-
-contract Action {
-    address public parent;
-    uint256 public data;
-
-    constructor(address _parent, uint256 _data) {
-        parent = _parent;
-        data = _data;
     }
 }
