4676a9ce18fa0986c49d0a99e6b246c768091a7c
Mark Roddy <mroddy@deloitte.com>
2022-02-20 00:36:54 -0700

    Implement code to create action

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,38 @@ import ActionList from "../components/actions/ActionList";
 import { getFeaturedActions } from "../mock-data/actions";
 import ActionForm from "../components/actions/ActionForm";
 import Button from "../components/ui/Button";
+// Ethers for web3
+import { ethers, Wallet } from "ethers";
+import actionFactory from "../artifacts/contracts/ActionFactory.sol/ActionFactory.json";
+import { EthersProvider } from "@ethers-react/system";
 
 const HomePage: NextPage = () => {
-  const createTopLevelAction = () => {
-    console.log("creating action");
+  // A Web3Provider wraps a standard Web3 provider, which is
+  // what MetaMask injects as window.ethereum into each page
+  // const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const localProvider = ethers.getDefaultProvider("http://127.0.0.1:8545/");
+  const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+  const tempWalletKey =
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  // Create Temp wallet based on hard coded
+  const tempWallet = new Wallet(tempWalletKey, localProvider);
+  // Create new contract
+  const contract = new ethers.Contract(
+    contractAddress,
+    actionFactory.abi,
+    localProvider
+  );
+
+  const connectedContract = contract.connect(tempWallet);
+
+  const createTopLevelAction = async () => {
+    const result = await connectedContract.createAction(contractAddress, 0);
+    console.log("Transaction Hash", result);
   };
 
   const featuredActions = getFeaturedActions();
-  console.log("Actions", featuredActions);
+  // console.log("Actions", featuredActions);
   return (
     <div className={styles.container}>
       <Head>
@@ -33,6 +57,8 @@ const HomePage: NextPage = () => {
         <div className="max-w-7xl mx-auto">
           <ActionForm />
         </div>
+
+        <button onClick={createTopLevelAction}>CreateAction</button>
 
         <ActionList actions={featuredActions} />
         <div className={styles.grid}></div>
