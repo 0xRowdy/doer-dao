d277f1a1a5f4c5bf92fc4f8d5df760b6f449e0a8
Roddy <mroddy@usd11mroddy2.lan>
2022-03-19 20:27:13 -0600

    Resolved conflicts from stash

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,94 @@ import Button from "../components/ui/Button";
 // Ethers for web3
 import { ethers, Signer, Wallet } from "ethers";
 import actionFactory from "../artifacts/contracts/ActionFactory.sol/ActionFactory.json";
-import detectEthereumProvider from "@metamask/detect-provider";
-import Onboard from "bnc-onboard";
+import Onboard from "@web3-onboard/core";
+// Onboard for connecting to wallets
+import {
+  useConnectWallet,
+  useSetChain,
+  useWallets,
+  init,
+} from "@web3-onboard/react";
+import injectedModule from "@web3-onboard/injected-wallets";
+import trezorModule from "@web3-onboard/trezor";
+import ledgerModule from "@web3-onboard/ledger";
+import walletConnectModule from "@web3-onboard/walletconnect";
+import walletLinkModule from "@web3-onboard/walletlink";
+import portisModule from "@web3-onboard/portis";
+import fortmaticModule from "@web3-onboard/fortmatic";
+import torusModule from "@web3-onboard/torus";
+import keepkeyModule from "@web3-onboard/keepkey";
+
+const injected = injectedModule();
+const walletLink = walletLinkModule();
+const walletConnect = walletConnectModule();
+const portis = portisModule({
+  apiKey: "b2b7586f-2b1e-4c30-a7fb-c2d1533b153b",
+});
+
+const fortmatic = fortmaticModule({
+  apiKey: "pk_test_886ADCAB855632AA",
+});
+
+const torus = torusModule();
+const ledger = ledgerModule();
+const keepkey = keepkeyModule();
+
+const trezorOptions = {
+  email: "test@test.com",
+  appUrl: "https://www.blocknative.com",
+};
+
+const trezor = trezorModule(trezorOptions);
+
+const web3Onboard = init({
+  wallets: [
+    ledger,
+    trezor,
+    walletConnect,
+    keepkey,
+    walletLink,
+    injected,
+    fortmatic,
+    portis,
+    torus,
+  ],
+  chains: [
+    {
+      id: "0x1",
+      token: "ETH",
+      label: "Ethereum Mainnet",
+      rpcUrl: "https://mainnet.infura.io/v3/ababf9851fd845d0a167825f97eeb12b",
+    },
+    {
+      id: "0x3",
+      token: "tROP",
+      label: "Ethereum Ropsten Testnet",
+      rpcUrl: "https://ropsten.infura.io/v3/ababf9851fd845d0a167825f97eeb12b",
+    },
+    {
+      id: "0x4",
+      token: "rETH",
+      label: "Ethereum Rinkeby Testnet",
+      rpcUrl: "https://rinkeby.infura.io/v3/ababf9851fd845d0a167825f97eeb12b",
+    },
+    {
+      id: "0x89",
+      token: "MATIC",
+      label: "Matic Mainnet",
+      rpcUrl: "https://matic-mainnet.chainstacklabs.com",
+    },
+  ],
+  appMetadata: {
+    name: "Blocknative",
+    icon: "<svg><svg/>",
+    description: "Demo app for Onboard V2",
+    recommendedInjectedWallets: [
+      { name: "MetaMask", url: "https://metamask.io" },
+      // { name: "Coinbase", url: "https://wallet.coinbase.com/" },
+    ],
+  },
+});
 
 const HomePage: NextPage = () => {
   const [loginState, setLoginState] = useState({
